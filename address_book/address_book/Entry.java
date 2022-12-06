745cb4ca9b9ca6c1c072a22c6ff8d3935b07d391
Richard Murphy <richinatl@gmail.com>
2022-12-06 09:12:53 -0500

    added entry class, added main method

---
+++ address_book/address_book/Entry.java
@@ -0,0 +1,32 @@
+package address_book;
+
+
+	public class Entry {
+		private String name;
+	    private String address;
+	    private String phoneNumber;
+	    
+	 public void setName(String privateName) {
+		 name = privateName;
+	 }
+	 
+	 public String getName() {
+		 return name;
+	 }
+	 
+	 public void setAddress(String privateAddress) {
+		 address = privateAddress;
+	 }
+	 
+	 public String getAddress() {
+		 return address;
+	 }
+	 
+	 public void setPhoneNumber(String privatePhoneNumber) {
+		 phoneNumber = privatePhoneNumber;
+	 }
+	 
+	 public String getPhoneNumber(String privatePhoneNumber) {
+		 return phoneNumber;
+	 }
+}
